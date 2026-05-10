0412be2282eb2f5daac180a8515a62a34c0b93c9
Dormolin <syntarou@maia.eonet.ne.jp>
2026-05-10 11:59:59 +0900

    fix: url base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/',
+  base: 'https://dormolin.github.io/randomConvex/',
   resolve: {
     alias: {
       '@': path.resolve('/src'),
