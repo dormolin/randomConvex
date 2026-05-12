2196cfb5b7304298c42354821741064c1139d631
Dormolin <syntarou@maia.eonet.ne.jp>
2026-05-12 23:27:39 +0900

    feat: save dlg

--- src/Menu.svelte
+++ src/Menu.svelte
@@ -1,46 +1,53 @@
 <script lang="ts">
     import * as fabric from 'fabric';
     import { RandomConvex, RandomConvexOptions } from './RandomConvex';
-    let { canvas, app, options, ...props } 
+    let { canvas, app, options, ...props }
         : { canvas?: fabric.Canvas, app?: RandomConvex, options?: RandomConvexOptions } = $props();
+    let dlg: HTMLDialogElement;
     let saveFormat: 'jpeg' | 'svg' = $state('jpeg');
+    let saveQuality = $state(0.95);
 
     const onGenerate = (event: Event) => {
         if (!app) return;
-        app._generateConvex(app.canvas, options?? new RandomConvexOptions())
-    }
+        app._generateConvex(app.canvas, options?? new RandomConvexOptions());
+    };
 
     const onClear = (event: Event) => {
         if (!canvas) return;
-        const bg = canvas.backgroundColor
-        canvas.clear()
+        const bg = canvas.backgroundColor;
+        canvas.clear();
         canvas.backgroundColor = bg;
         canvas.requestRenderAll();
-    }
+    };
 
     const onDelete = (event: Event) => {
-        const tobeRemoved = canvas?.getActiveObjects()
+        const tobeRemoved = canvas?.getActiveObjects();
         if (!tobeRemoved) return;
-        canvas?.remove(...tobeRemoved)
-        canvas?.discardActiveObject()
-        canvas?.requestRenderAll()
-    }
+        canvas?.remove(...tobeRemoved);
+        canvas?.discardActiveObject();
+        canvas?.requestRenderAll();
+    };
+
+    const onSaveDlgOpen = (event: Event) => dlg.showModal();
 
     const onSave = (event: Event) => {
         if (!canvas) return;
-        const a = document.createElement('a')
+        const a = document.createElement('a');
         switch (saveFormat) {
         case 'jpeg':
-            a.href = canvas?.toDataURL({format:'jpeg', quality: 0.95, multiplier: 1.0})
-            a.download = 'randomConvexOutput.jpg'
-            break
+            a.href = canvas?.toDataURL({format:'jpeg', quality: saveQuality, multiplier: 1.0});
+            a.download = 'randomConvexOutput.jpg';
+            break;
         case 'svg':
-            a.href = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(canvas?.toSVG())
-            a.download = 'randomConvexOutput.svg'
-            break
+            a.href = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(canvas?.toSVG());
+            a.download = 'randomConvexOutput.svg';
+            break;
         }
-        a.click()
-    }
+        a.click();
+        dlg.close();
+    };
+
+    const onCancelSave = (event: Event) => dlg.close();
 </script>
 
 
@@ -48,14 +55,23 @@
     <button type="button" onclick={onGenerate}>generate</button>
     <button type="button" onclick={onClear}>clear</button>
     <button type="button" onclick={onDelete}>delete</button>
-    <button type="button" onclick={onSave}>save</button>
+    <button type="button" onclick={onSaveDlgOpen}>save</button>
+</form>
+<dialog bind:this={dlg}>
     <label>
         jpeg <input type="radio" name="saveFormat" value="jpeg" checked bind:group={saveFormat}>
     </label>
     <label>
         svg <input type="radio" name="saveFormat" value="svg" bind:group={saveFormat}>
     </label>
-</form>
+    <br>
+    {#if saveFormat==='jpeg'}
+        Quality: <input type="range" min="0.7" max="1.0" step="0.01" bind:value={saveQuality} />{saveQuality}
+    {/if}
+    <hr>
+    <button type="button" onclick={onCancelSave}>Cancel</button>
+    <button type="button" onclick={onSave}>OK</button>
+</dialog>
 
 
 <style>
